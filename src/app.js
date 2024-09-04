e6d20f0f237aa31a7732ba90e65674158a1b4ef7
Cristopher5917K <garcacristopher3@gmail.com>
2024-09-04 16:45:06 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,26 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+window.onload = () => {
+  document.querySelector("#excuse").innerHTML = generateExcuse();
+};
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+let generateExcuse = () => {
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  let quien = Math.floor(Math.random() * who.length);
+  let verbo = Math.floor(Math.random() * action.length);
+  let que = Math.floor(Math.random() * what.length);
+  let cuando = Math.floor(Math.random() * when.length);
+
+  return who[quien] + "" + action[verbo] + "" + what[que] + "" + when[cuando];
 };
